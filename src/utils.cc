21a740348e38a21511f72bcd0a8fb4b4998cb585
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-07-28 15:07:22 +1200

    Add copyright headers

--- src/utils.cc
+++ src/utils.cc
@@ -1,3 +1,16 @@
+/*
+ *                This source code is part of
+ *
+ *                          HelFEM
+ *                             -
+ * Finite element methods for electronic structure calculations on small systems
+ *
+ * Copyright (c) 2018 Susi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or * modify it under the terms
+ * of the GNU General Public License * as published by the Free Software Foundation; either
+ * version 2 * of the License, or (at your option) any later version.
+ */
 #include <helfem>
 
 arma::vec helfem::utils::get_grid(double rmax, int num_el, int igrid, double zexp) {
