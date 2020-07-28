69b262691a11ab7c1f51974fad92d50b27f2b0ab
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-07-28 15:27:47 +1200

    Fix tests

--- test/armadillo.cc
+++ test/armadillo.cc
@@ -1,8 +1,9 @@
 #include <armadillo>
 
-int main(int argc, char * argv[]) {
-    arma::Col<int> v(3);
-    v.fill(3);
-    if(sum(v) != 9) return 1;
-    return 0;
+int main(int argc, char *argv[]) {
+  arma::Col<int> v(3);
+  v.fill(3);
+  if (sum(v) != 9)
+    return 1;
+  return 0;
 }
--- test/runtests.cc
+++ test/runtests.cc
@@ -1,18 +1,17 @@
-#include <helfem>
-#include <helfem_utils>
 #include <armadillo>
+#include <helfem>
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-    helfem::hw();
+int main(int argc, char *argv[]) {
+  helfem::hw();
 
-    // Call helfem::utils::get_grid
-    auto v = helfem::utils::get_grid(5.0, 10, 1, 0.0);
-    cout << "Linear grid:" << endl << v << endl;
+  // Call helfem::utils::get_grid
+  auto v = helfem::utils::get_grid(5.0, 10, 1, 0.0);
+  cout << "Linear grid:" << endl << v << endl;
 
-    v = helfem::utils::get_grid(40.0, 20, 4, 2.0);
-    cout << "Exponential grid:" << endl << v << endl;
+  v = helfem::utils::get_grid(40.0, 20, 4, 2.0);
+  cout << "Exponential grid:" << endl << v << endl;
 
-    return 0;
+  return 0;
 }
